a95a95fbc8ef0d67cd75498d22251d0821761e7c
maryjane <jhmin1993@gmail.com>
2019-12-06 22:29:08 +0900

    Solve the problem

--- src/main/java/defanging_an_ip_address/DefangingAnIpAddress.java
+++ src/main/java/defanging_an_ip_address/DefangingAnIpAddress.java
@@ -2,6 +2,14 @@ package defanging_an_ip_address;
 
 public class DefangingAnIpAddress {
     public String defangIPaddr(String address) {
-        return null;
+        String resultAddr = "";
+        String[] addrArr = address.split("\\.");
+        for (int i = 0; i < addrArr.length; i++) {
+            if (i == addrArr.length-1)
+                resultAddr = resultAddr + addrArr[i];
+            else
+                resultAddr = resultAddr + addrArr[i] + "[.]";
+        }
+        return resultAddr;
     }
 }
