124a4cb5ff0136af52aa3ee09b08f5b41c2bab49
maryjane <jhmin1993@gmail.com>
2019-12-06 22:39:45 +0900

    solve the problem in another way

--- src/main/java/defanging_an_ip_address/DefangingAnIpAddress.java
+++ src/main/java/defanging_an_ip_address/DefangingAnIpAddress.java
@@ -1,15 +1,19 @@
 package defanging_an_ip_address;
 
 public class DefangingAnIpAddress {
+    public static void main(String[] args) {
+        DefangingAnIpAddress defangingAnIpAddress = new DefangingAnIpAddress()  ;
+        System.out.println(defangingAnIpAddress.defangIPaddr("1.1.1.1"));
+    }
     public String defangIPaddr(String address) {
-        String resultAddr = "";
+        StringBuilder resultAddr = new StringBuilder();
         String[] addrArr = address.split("\\.");
         for (int i = 0; i < addrArr.length; i++) {
             if (i == addrArr.length-1)
-                resultAddr = resultAddr + addrArr[i];
+                resultAddr.append(addrArr[i]);
             else
-                resultAddr = resultAddr + addrArr[i] + "[.]";
+                resultAddr.append(addrArr[i]).append("[.]");
         }
-        return resultAddr;
+        return resultAddr.toString();
     }
 }
