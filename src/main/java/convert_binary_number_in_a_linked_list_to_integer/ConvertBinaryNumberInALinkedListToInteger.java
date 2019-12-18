bcde1250d97d6facc87d77e73ad837482d1877a6
maryjane <jhmin1993@gmail.com>
2019-12-18 21:11:24 +0900

    Solve the problem
    - Runtime: 0 ms (100%)
    - Memory Usage: 34.4 MB (100%)

---
+++ src/main/java/convert_binary_number_in_a_linked_list_to_integer/ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,77 @@
+package convert_binary_number_in_a_linked_list_to_integer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConvertBinaryNumberInALinkedListToInteger {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+
+            int ret = new ConvertBinaryNumberInALinkedListToInteger().getDecimalValue(head);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public int getDecimalValue(ListNode head) {
+        ListNode current = head;
+        int decimalVal;
+        StringBuilder sb = new StringBuilder();
+
+        while (current.next != null) {
+            decimalVal = current.val;
+            sb.append(decimalVal);
+            current = current.next;
+        }
+        sb.append(current.val);
+
+        decimalVal = Integer.parseInt(sb.toString(), 2);
+
+        return decimalVal;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
